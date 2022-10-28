dbaddf2e5a7d710ee2ee38683c657aafe8327c00
OksanaVovk <oksvovk85@gmail.com>
2022-10-28 14:44:19 +0300

    finish

--- schemas/contacts.js
+++
@@ -1,8 +0,0 @@
-const Joi = require("joi");
-const contactSchema = Joi.object({
-  name: Joi.string().alphanum().min(3).required(),
-  email: Joi.string().email().required(),
-  phone: Joi.number().integer().required(),
-});
-
-module.exports = contactSchema;
